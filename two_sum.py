9dd72c35b7c8fd21b41332b48ad97db77c7519f6
IhyeonKIM <100825090+IhyeonKIM@users.noreply.github.com>
2023-03-05 13:59:31 +0000

    230305

--- two_sum.py
+++ two_sum.py
@@ -35,3 +35,46 @@ def TwoSum( nums , target):
   return False 
   
 print(TwoSum([1,2,3,4,6], 10))
+
+
+
+##### LeetCode 
+
+# 참고풀이
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        d = {}
+        for i, j in enumerate(nums):
+            r = target - j
+            if r in d: return [d[r], i]
+            d[j] = i
+
+
+# 풀었는데 Case3이 해결안됨.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums1 = nums.copy()
+        nums.sort()
+        i = 0
+        j = len(nums)-1 
+
+        while i<j :
+            if nums[i] + nums[j] == target :
+                a = nums[i]
+                b = nums[j]
+                ans1 = nums1.index(a)
+                ans2 = nums1.index(b)
+                return ans1, ans2
+
+            elif nums[i] + nums[j] > target :
+                j-=1
+            elif nums[i] + nums[j] < target : 
+                i+=1
+
+
+
+
+
